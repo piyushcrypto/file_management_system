d996df8a37b6ffe0df00552cd2ec00a3c64e0915
Piyush Ranjan <piyush.ranjan@planetspark.in>
2024-12-18 18:56:11 +0530

    adding s3 configs

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -16,17 +16,20 @@ class FileUploadsController < ApplicationController
   
     if params[:file_upload][:file].present?
       file = params[:file_upload][:file]
-      @file_upload.save
-
-      if @file_upload.file.attached?
-        file_url = url_for(@file_upload.file)  
-        @file_upload.update(file_url: file_url)
-      end
-  
-      if @file_upload.save
-        redirect_to file_uploads_path, notice: "File uploaded successfully."
+      
+      file_url = upload_to_s3(file)
+      
+      if file_url
+        @file_upload.file_url = file_url
+        @file_upload.save
+        
+        if @file_upload.save
+          redirect_to file_uploads_path, notice: "File uploaded successfully."
+        else
+          render :new
+        end
       else
-        render :new
+        render :new, alert: "File upload failed."
       end
     else
       render :new, alert: "Please select a file to upload."
@@ -48,22 +51,28 @@ class FileUploadsController < ApplicationController
   end
 
   def upload_to_s3(file)
-    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+    s3 = Aws::S3::Resource.new(
+      region: ENV['AWS_REGION'],
+      endpoint: ENV['AWS_S3_ENDPOINT']
+    )
     bucket = s3.bucket(ENV['AWS_BUCKET_NAME'])
-
     file_name = "#{Time.now.to_i}_#{file.original_filename}"
     
     obj = bucket.object("uploads/#{current_user.id}/#{file_name}")
     obj.put(body: file.tempfile, acl: 'public-read') 
-    
+  
     obj.public_url
   rescue Aws::S3::Errors::ServiceError => e
     Rails.logger.error "S3 Upload Error: #{e.message}"
     nil
-  end
+  end    
 
   def delete_from_s3(file_upload)
-    s3 = Aws::S3::Client.new(region: ENV['AWS_REGION'])
+    s3 = Aws::S3::Client.new(
+      region: ENV['AWS_REGION'],
+      endpoint: ENV['AWS_S3_ENDPOINT'] # Add the endpoint here
+    )
+    
     if file_upload.file_url.present?
       file_key = "uploads/#{file_upload.user.id}/#{File.basename(file_upload.file_url)}"
       s3.delete_object(
@@ -76,5 +85,6 @@ class FileUploadsController < ApplicationController
   rescue Aws::S3::Errors::ServiceError => e
     Rails.logger.error "S3 Delete Error: #{e.message}"
   end
+  
   
 end
