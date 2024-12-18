f5a15e08c3d6cf4e16a21510544977eb34a0e8a1
Piyush Ranjan <piyush.ranjan@planetspark.in>
2024-12-18 15:40:58 +0530

    additional dependency changes

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,9 @@ require 'aws-sdk-s3'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
 module FileManagementSystem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
   end
 end
